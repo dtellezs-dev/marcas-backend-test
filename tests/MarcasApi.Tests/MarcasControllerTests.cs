db9527af0cd8d8602ff5e9f5d3eebb9590833e09
David Tellez <ing.davidtellez@gmail.com>
2025-10-23 01:20:04 -0600

    Marcas Controller Test updated

--- tests/MarcasApi.Tests/MarcasControllerTests.cs
+++ tests/MarcasApi.Tests/MarcasControllerTests.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;  // ? ESTE FALTA
+using System.Threading.Tasks;  
 using Microsoft.EntityFrameworkCore;
 using MarcasApi.Controllers;
 using MarcasApi.Data;
